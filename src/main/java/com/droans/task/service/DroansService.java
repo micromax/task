f5a62433113c1d7f4bdc5596ee197f0de0dbb1a4
Mohamed Alaa <m.elqrwash@gmail.com>
2022-11-20 19:03:55 +0200

    implements droan service & methods

--- src/main/java/com/droans/task/service/DroansService.java
+++ src/main/java/com/droans/task/service/DroansService.java
@@ -2,8 +2,8 @@ package com.droans.task.service;
 
 import com.droans.task.domain.Drons;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.awt.print.Pageable;
 import java.util.Optional;
 
 public interface DroansService {
@@ -13,6 +13,7 @@ public interface DroansService {
 
 
     Drons update(Drons droan);
+    Optional<Drons> partialUpdate(Drons droan);
 
     Page<Drons> findAll(Pageable pageable);
 
--- src/main/java/com/droans/task/service/impl/DroansServiceImpl.java
+++ src/main/java/com/droans/task/service/impl/DroansServiceImpl.java
@@ -4,16 +4,18 @@ import com.droans.task.domain.Drons;
 import com.droans.task.repository.DonsRepository;
 import com.droans.task.service.DroansService;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.awt.print.Pageable;
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @Transactional
 public class DroansServiceImpl implements DroansService {
 
+    private static final int MAX_DROAN = 10;
     private final DonsRepository donsRepository;
 
     public DroansServiceImpl(DonsRepository donsRepository) {
@@ -23,26 +25,69 @@ public class DroansServiceImpl implements DroansService {
 
     @Override
     public Drons save(Drons droan) {
-        return donsRepository.save(droan);
+        List<Drons> lists =  donsRepository.findAll();
+        if(lists.size() < MAX_DROAN){
+            return donsRepository.save(droan);
+        }else {
+            return  droan;
+        }
+
     }
 
     @Override
     public Drons update(Drons droan) {
-        return null;
+        return donsRepository.save(droan);
     }
 
     @Override
-    public Page<Drons> findAll(Pageable pageable) {
-        return null;
+    public Optional<Drons> partialUpdate(Drons drons)
+    {
+        return donsRepository
+                .findById(drons.getId())
+                .map(existingAppUsers -> {
+                    if (drons.getModel() != null) {
+                        existingAppUsers.setModel(drons.getModel());
+                    }
+
+
+
+                    if (drons.getBatteryCapacity() != -1) {
+                        existingAppUsers.setBatteryCapacity(drons.getBatteryCapacity());
+                    }
+                    if (drons.getWeightLimit() != -1) {
+                        existingAppUsers.setWeightLimit(drons.getWeightLimit());
+                    }
+                    if (drons.getState() != null) {
+                        existingAppUsers.setState(drons.getState());
+                    }
+
+                    if (drons.getBatteryCapacity() != -1) {
+                        existingAppUsers.setBatteryCapacity(drons.getBatteryCapacity());
+                    }
+
+                    if (drons.getSerial() != 0) {
+                        existingAppUsers.setSerial(drons.getSerial());
+                    }
+
+                    return existingAppUsers;
+                })
+                .map(donsRepository::save);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<Drons> findAll(Pageable p)
+    {
+        return donsRepository.findAll(p);
     }
 
     @Override
     public Optional<Drons> findOne(Long id) {
-        return Optional.empty();
+        return donsRepository.findById(id);
     }
 
     @Override
     public void delete(Long id) {
-
+        donsRepository.deleteById(id);
     }
 }
